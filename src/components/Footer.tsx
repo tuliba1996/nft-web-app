3885be64c618a41ed78d82923214a5beb85d4a1e
Duan <duancba@gmail.com>
2022-05-05 11:41:22 +0700

    fix target img

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -18,16 +18,25 @@ export const Footer = () => {
         </Text>
       </div>
       <div className="footer-icon-container">
-        <a href="https://github.com/tuliba1996/nft-web-app" target="_blank">
+        <a
+          href="https://github.com/tuliba1996/nft-web-app"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Image src={imgGithub.src} w={8} h={8} marginInline={2} />
         </a>
         <a
           href="https://www.linkedin.com/in/duan-huynh-ba447313a/"
           target="_blank"
+          rel="noreferrer"
         >
           <Image src={imgLinkedin.src} w={8} h={8} marginInline={2} />
         </a>
-        <a href="https://twitter.com/tuliba1996" target="_blank">
+        <a
+          href="https://twitter.com/tuliba1996"
+          target="_blank"
+          rel="noreferrer"
+        >
           <Image src={imgTwitter.src} w={8} h={8} marginInline={2} />
         </a>
       </div>
